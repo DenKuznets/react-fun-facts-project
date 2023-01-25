9e07ff0fe70dc96591d1488b701873eb555f32ee
Denis Kuznetsov workPC <denis.kyznecov@gmail.com>
2023-01-25 15:05:08 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import './App.css'
+
+import logoUrl from './assets/react.svg'
+
+function Header() {
+  return (
+    <header className='header'>
+      <nav className='nav'>
+        <img src={logoUrl} width="40px" alt='react-logo'/>
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <div>
+      <h1 className='main-h1'>Reasons I'm excited to learn React</h1>
+      <ol className='main-ol'>
+        <li>
+          It's a popular library, so I'll be able to fit in with the cool kids!
+        </li>
+        <li>I'm more likely to get a job as a developer if I know React</li>
+      </ol>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2021 Kuznetsov development. All rights reserved.</small>
+    </footer>
+  );
+}
+
+function App() {  
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
